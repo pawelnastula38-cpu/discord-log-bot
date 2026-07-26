026be2e4df1e2e794f6cd14fce1c64543ae56208
pawelnastula38-cpu <pawelnastula38@gmail.com>
2026-07-26 04:42:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,28 @@
 import discord
 from discord.ext import commands
-
 import os
 
 TOKEN = os.getenv("TOKEN")
-
-bot.run(TOKEN)
 
 KANAL_LOGOW = 1417656464209608704
 
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
 
 @bot.event
 async def on_ready():
-    print(f"Bot działa jako {bot.user}")
+    print(f"✅ Bot działa jako {bot.user}")
     print("Serwery:", bot.guilds)
 
 @bot.event
 async def on_member_remove(member):
     kanal = bot.get_channel(KANAL_LOGOW)
+
     if kanal:
         await kanal.send(
             f"📤 **{member.name}** opuścił serwer."
